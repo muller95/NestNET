3d1ed58341801877eeb6a94fffa1f12996f6de59
muller95 <muller95@yandex.ru>
2017-05-26 22:35:03 +0300

    Generate SVG results via awk legacy

--- output-parser/Program.cs
+++ output-parser/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace OutputParser
 {
@@ -8,30 +9,48 @@ namespace OutputParser
         static string prefixSet1 = "/home/vadim/SvgFiles/SvgSamples/set1/";        
         static string prefixSet2 = "/home/vadim/SvgFiles/SvgSamples/set2/";
         static string[] set1 = { "фигурки1.svg",  "фигурки2.svg",  "фигурки3.svg",  "фигурки4.svg",  "фигурки5.svg"};
-        
+
+
         static void Main(String[] args)
         {
             StreamReader sr = new StreamReader("set1");
-            StreamWriter result = new StreamWriter("set1out.svg");
 
-            result.WriteLine("<svg>");
+            ProcessStartInfo startInfo;        
+            int count = 0;
+            startInfo = new ProcessStartInfo ("gawk", string.Format (
+                "-f backend.awk -v width=\"1000\" -v height=\"1000\" -v outfile=\"{0}\"", String.Format("out/{0}.svg", count)));
+			startInfo.RedirectStandardOutput = false;	
+			startInfo.RedirectStandardInput = true;
+			startInfo.UseShellExecute = false;
+			Process gawkout = new Process ();
+			gawkout.StartInfo = startInfo;
+			gawkout.Start ();
+
             while (!sr.EndOfStream) {
                 string str1 = sr.ReadLine();
                 if (str1 == ":")
+                {
+                    count++;
+                    gawkout.StandardInput.Close();
+                    startInfo = new ProcessStartInfo ("gawk", string.Format (
+                        "-f backend.awk -v width=\"1000\" -v height=\"1000\" -v outfile=\"{0}\"", String.Format("out/{0}.svg", count)));
+                    startInfo.RedirectStandardOutput = false;	
+                    startInfo.RedirectStandardInput = true;
+                    startInfo.UseShellExecute = false;
+                    gawkout = new Process ();
+                    gawkout.StartInfo = startInfo;
+                    gawkout.Start ();
                     continue;
+                }
                 
                 string str2 = sr.ReadLine();
 
                 int id = Convert.ToInt32(str1);
-
-                Console.WriteLine("str1 " +  str1);
-                Console.WriteLine("str2 " + str2);
-
-                StreamReader figReader = new StreamReader(prefixSet1 + set1[id]);
-                result.Write(figReader.ReadToEnd().Replace("<svg>", String.Format("<g transform=\"{0}\"", str2).Replace("</svg>", "</g>")));
+                Console.WriteLine(prefixSet1 + set1[id]);
+                gawkout.StandardInput.WriteLine(prefixSet1 + set1[id]);
+                gawkout.StandardInput.WriteLine(str2);
+                gawkout.StandardInput.WriteLine(":");
             }
-            result.WriteLine("</svg>");            
-            result.Close();
         }
     }
 }
